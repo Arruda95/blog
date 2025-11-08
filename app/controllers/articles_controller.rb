b178e67453142c255920dac6df64db127c311b69
arruda95 <two.vitor@gmail.com>
2025-11-07 22:36:46 -0300

    Exclui um Article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,11 +1,13 @@
 class ArticlesController < ApplicationController
+
+  before_action :set_article, only: %i[show edit update destroy]
+
+
   def index
     @articles = Article.all
   end
 
-  def show
-    @article = Article.find(params[:id])
-  end
+  def show;  end
 
   def new
     @article = Article.new
@@ -14,29 +16,36 @@ class ArticlesController < ApplicationController
   def create
     @article = Article.new(article_params)
     if @article.save
-      redirect_to @article, notice: 'Article criado com sucesso!'
+      redirect_to @article
     else
       render :new
     end
   end
 
-  def edit
-    @article = Article.find(params[:id])
-  end
+  def edit;  end
 
   def update
-    @article = Article.find(params[:id])
     if @article.update(article_params)
-      redirect_to @article, notice: 'Article atualizado com sucesso!'
+      redirect_to @article
     else
       render :edit
     end
   end
 
 
+  def destroy
+  @article.destroy
+  redirect_to root_path
+end
+
   private
 
   def article_params
     params.require(:article).permit(:title, :body)
   end
+
+  def set_article
+    @article = Article.find(params[:id])
+  end
+
 end
