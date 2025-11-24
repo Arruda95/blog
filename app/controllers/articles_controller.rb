ba63c6fd864db4b3841d3b9c7c868c84421c05ff
arruda95 <two.vitor@gmail.com>
2025-11-24 18:33:20 -0300

    Update:

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,9 +4,12 @@ class ArticlesController < ApplicationController
 
 
   def index
-    current_page = (params[:page] || 1).to_i
+    @highlights = Article.desc_order.first(3)
+    excluded_ids = @highlights.pluck(:id)
 
-    @articles = Article.order(created_at: :desc).page(current_page).per(2)
+    @articles = Article.desc_order  
+                       .where.not(id: excluded_ids)
+                       .page(params[:page])
   end
 
   def show;  end
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,9 @@
 class Article < ApplicationRecord
 validates :title, presence: true, length: { minimum: 5 }
 validates :body, presence: true, length: { minimum: 10 }
+
+paginates_per 1
+
+scope :desc_order, -> { order(created_at: :desc) }
+scope :excluded_ids, ->(ids) { where.not(id: ids) }
 end
